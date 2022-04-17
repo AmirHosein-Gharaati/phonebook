1867d2e1486bc50a564b2988c9f2bef2e9803f15
Amir <amirgh1380@gmail.com>
2022-04-17 10:02:31 +0430

    removed styled files

--- src/app/modules/home/components/contact-detail/contact-detail.component.ts
+++ src/app/modules/home/components/contact-detail/contact-detail.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrls: ['./contact-detail.component.scss']
 })
 export class ContactDetailComponent implements OnInit {
 
--- src/app/modules/home/pages/add/add.component.ts
+++ src/app/modules/home/pages/add/add.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
 
