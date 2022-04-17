5f8d544576dd3c68d43102b9379e33e256a09002
Amir <amirgh1380@gmail.com>
2022-04-17 09:20:42 +0430

    initial setup for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { ShortenPipe } from './shared/pipes/shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoaderComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
