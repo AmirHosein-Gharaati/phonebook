7da30a22ffc176e2905ed22890ee3ae2240f18a0
Amir <amirgh1380@gmail.com>
2022-04-18 14:26:41 +0430

    adding route for editing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
         path: ':id',
         component: ContactDetailComponent,
       },
+      {
+        path: ':id/edit',
+        component: AddComponent,
+      },
     ],
   },
 ];
