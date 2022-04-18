b05fe33256e8d7279a022dc0f3de1b400c7afdb4
Amir <amirgh1380@gmail.com>
2022-04-18 11:33:11 +0430

    initialing component for viewing it

--- src/app/modules/home/components/contact-item/contact-item.component.ts
+++ src/app/modules/home/components/contact-item/contact-item.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from 'src/app/shared/models/contact.model';
 
 @Component({
   selector: 'app-contact-item',
   templateUrl: './contact-item.component.html',
 })
 export class ContactItemComponent implements OnInit {
+  @Input() contact: Contact;
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import { ContactService } from 'src/app/core/services/contact.service';
+import { Contact } from 'src/app/shared/models/contact.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   faMagnifyingGlass = faMagnifyingGlass;
+  contacts: Contact[] = [];
+  contactsSubscription: Subscription; // ?
 
-  constructor() {}
+  constructor(private contactService: ContactService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contactsSubscription = this.contactService.contactsChanged.subscribe(
+      (contacts: Contact[]) => {
+        this.contacts = contacts;
+      }
+    );
+
+    this.contacts = this.contactService.getContacts();
+    console.log(this.contacts);
+  }
+
+  ngOnDestroy(): void {
+    this.contactsSubscription.unsubscribe();
+  }
 }
