0bfc1bfd607af24fe8ba13e39b8aee4672d33d21
Amir <amirgh1380@gmail.com>
2022-04-18 09:00:10 +0430

    routing for items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailComponent } from './modules/home/components/contact-detail/contact-detail.component';
 import { AddComponent } from './modules/home/pages/add/add.component';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 
@@ -7,6 +8,12 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [
+      {
+        path: ':id',
+        component: ContactDetailComponent,
+      },
+    ],
   },
   {
     path: 'add',
