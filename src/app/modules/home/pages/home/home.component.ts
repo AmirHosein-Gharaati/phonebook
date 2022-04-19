66aaff22c71135f5c106571af89d832deebf2a26
Amirhosein Gharaati <amirgh1380@gmail.com>
2022-04-19 12:58:31 +0430

    search debounce time

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import { ContactService } from 'src/app/core/services/contact.service';
@@ -10,9 +17,11 @@ import { Contact } from 'src/app/shared/models/contact.model';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
   faMagnifyingGlass = faMagnifyingGlass;
   contacts: Contact[] = [];
   contactsSubscription: Subscription;
+  searchInputSubscription: Subscription;
   searchText = '';
 
   constructor(private contactService: ContactService) {}
@@ -25,9 +34,25 @@ export class HomeComponent implements OnInit, OnDestroy {
     );
 
     this.contacts = this.contactService.getContacts();
+
+    this.searchInputSubscription = fromEvent(
+      this.searchInput.nativeElement,
+      'keyup'
+    )
+      .pipe(
+        map((event: any) => {
+          return event.target.value;
+        }),
+        debounceTime(1000),
+        distinctUntilChanged()
+      )
+      .subscribe((text: string) => {
+        this.searchText = text;
+      });
   }
 
   ngOnDestroy(): void {
     this.contactsSubscription.unsubscribe();
+    this.searchInputSubscription.unsubscribe();
   }
 }
