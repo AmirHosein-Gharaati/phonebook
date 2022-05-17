9e5b00f5f51abfe53ce3cccee1b32cb9ead7da56
AmirHosein Gharaati <AmirHoseinG@systemgroup.net>
2022-05-17 09:15:58 +0430

    apiService in seperate module

--- src/app/core/http/api.service.ts
+++ src/app/core/http/api.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Contact } from 'src/app/shared/models/contact.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getContacts() {
+    return this.http.get<Contact[]>('http://localhost:5000/api/contact');
+  }
 }
