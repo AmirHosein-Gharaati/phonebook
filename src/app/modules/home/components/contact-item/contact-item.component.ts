000076406257bfb3dd41c58838acb3167f8e699b
AmirHosein Gharaati <AmirHoseinG@systemgroup.net>
2022-05-28 11:37:39 +0430

    default image url for contact item

--- src/app/modules/home/components/contact-item/contact-item.component.ts
+++ src/app/modules/home/components/contact-item/contact-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Person } from 'src/app/shared/models/person.model';
+import { IMAGE_URLS } from 'src/app/shared/image-urls.model';
 
 @Component({
   selector: 'app-contact-item',
@@ -8,6 +9,7 @@ import { Person } from 'src/app/shared/models/person.model';
 export class ContactItemComponent implements OnInit {
   @Input() person: Person;
   selectedPerson: Person;
+  IMAGES = IMAGE_URLS;
 
   ngOnInit(): void {}
 }
