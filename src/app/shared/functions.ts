1186e33217d5a2e0289604aa323f0a3966c25c04
AmirHosein Gharaati <AmirHoseinG@systemgroup.net>
2022-05-29 09:49:51 +0430

    get proper name for enums

--- src/app/shared/functions.ts
+++ src/app/shared/functions.ts
@@ -15,3 +15,18 @@ export function getProperImage(categoryType: string | number) {
       return IMAGE_URLS.DEFAULT;
   }
 }
+
+export function getProperName(categoryType: string | number) {
+  const type = Number(categoryType) as Categories;
+
+  switch (type) {
+    case Categories.PHONE_NUMBER:
+      return 'Phone Number';
+    case Categories.HOME_NUMBER:
+      return 'Home Number';
+    case Categories.EMAIL:
+      return 'Email';
+    default:
+      return '';
+  }
+}
